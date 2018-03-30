179a2e9509d62725a05504e9e019b2d9533298b8
EC2 Default User <ec2-user@ip-172-31-21-110.us-east-2.compute.internal>
2018-03-30 04:00:46 +0000

    Added git ignore

--- app/config.php
+++ app/config.php
@@ -2,12 +2,12 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://localhost/bojanglers'); # your base URL
-// define('BASE_URL','http://ec2-18-222-78-83.us-east-2.compute.amazonaws.com/bojanglers');
+// define('BASE_URL','http://localhost/bojanglers'); # your base URL
+define('BASE_URL','http://ec2-18-222-78-83.us-east-2.compute.amazonaws.com/bojanglers');
 
 define('DB_USER', 'root');
-define('DB_PASS', '');
-// define('DB_PASS', 'Bojanglers18');
+// define('DB_PASS', '');
+define('DB_PASS', 'Bojanglers18');
 
 define('DB_HOST', 'localhost');
 define('DB_DATABASE', 'chaplain');
