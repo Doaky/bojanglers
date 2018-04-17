219f2a89b935403f9612601cc242e655930ac27c
Daniel Ocheltree <djo96@vt.edu>
2018-04-17 11:21:25 -0400

    Updated config

--- app/config.php
+++ app/config.php
@@ -3,7 +3,7 @@
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
 // define('BASE_URL','http://localhost/bojanglers'); # your base URL
-define('BASE_URL','http://ec2-18-222-78-83.us-east-2.compute.amazonaws.com/bojanglers');
+define('BASE_URL','http://ec2-18-217-29-6.us-east-2.compute.amazonaws.com/bojanglers/');
 
 define('DB_USER', 'root');
 // define('DB_PASS', '');
